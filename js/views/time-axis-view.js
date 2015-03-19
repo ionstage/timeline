5718af24e427b731a204270cb84256851ea37f9b
nasitra <nasitra@users.noreply.github.com>
2015-03-19 21:46:05 +0900

    Add marker of the first day of each month on the time axis

--- js/views/time-axis-view.js
+++ js/views/time-axis-view.js
@@ -10,6 +10,39 @@
 
     var height = 31;
 
+    var monthMarkerViews = [];
+    var now = Date.now();
+    for (var di = daysAgo, dlen = daysAfter + 1; di < dlen; di++) {
+      var date = new Date(now + di * 86400000);
+      if (date.getDate() !== 1)
+        continue;
+      var month = date.getMonth() + 1;
+      var showYear = (month === 1 || month === 4 || month === 7 || month === 10);
+      var x = (di - daysAgo) * pixelsPerDay;
+      var width = showYear ? 60 : 20;
+      var text = showYear ? date.getFullYear() + '/' + month : month;
+      monthMarkerViews.push(m('g.month', [
+        m('line', {
+          x1: x,
+          y1: 0,
+          x2: x,
+          y2: height
+        }),
+        m('rect', {
+          x: x - (showYear ? 30 : 10),
+          y: 5,
+          width: width,
+          height: 20,
+          rx: 5,
+          ry: 5
+        }),
+        m('text', {
+          x: x,
+          y: 16
+        }, text)
+      ]));
+    }
+
     return m('div.time-axis', [
       m('svg', {
         width: (daysAfter - daysAgo + 1) * pixelsPerDay + 1,
@@ -43,7 +76,8 @@
           y: 0,
           width: '100%',
           height: height
-        })
+        }),
+        monthMarkerViews
       ])
     ]);
   };
