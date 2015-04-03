82e7abf7d4e368ff569f379965116cfb009087c3
nasitra <nasitra@users.noreply.github.com>
2015-04-03 21:56:12 +0900

    Refactor time-axis-view

--- js/views/time-axis-view.js
+++ js/views/time-axis-view.js
@@ -2,6 +2,10 @@
   'use strict';
   var app = global.app || {};
   var m = global.m;
+  var util = global.util;
+
+  var startOfDay = util.startOfDay;
+  var addDays = util.addDays;
 
   var timeAxisView = function(ctrl) {
     var daysAgo = ctrl.daysAgo();
@@ -9,39 +13,6 @@
     var pixelsPerDay = ctrl.pixelsPerDay();
 
     var height = 31;
-
-    var monthMarkerViews = [];
-    var now = Date.now();
-    for (var di = -daysAgo, dlen = daysAfter + 1; di < dlen; di++) {
-      var date = new Date(now + di * 86400000);
-      if (date.getDate() !== 1)
-        continue;
-      var month = date.getMonth() + 1;
-      var showYear = (month === 1 || month === 4 || month === 7 || month === 10);
-      var x = (di + daysAgo) * pixelsPerDay;
-      var width = showYear ? 60 : 20;
-      var text = showYear ? date.getFullYear() + '/' + month : month;
-      monthMarkerViews.push(m('g.month', [
-        m('line', {
-          x1: x,
-          y1: 0,
-          x2: x,
-          y2: height
-        }),
-        m('rect', {
-          x: x - (showYear ? 30 : 10),
-          y: 5,
-          width: width,
-          height: 20,
-          rx: 5,
-          ry: 5
-        }),
-        m('text', {
-          x: x,
-          y: 20
-        }, text)
-      ]));
-    }
 
     return m('div.time-axis.unselectable', {
       config: function(element, isInitialized) {
@@ -89,7 +60,7 @@
             context.pixelsPerDay = pixelsPerDay;
           }
         }),
-        monthMarkerViews,
+        monthMarkerViews(daysAgo, daysAfter, pixelsPerDay, height),
         m('rect.today', {
           x: daysAgo * pixelsPerDay,
           y: 0,
@@ -98,6 +69,42 @@
         })
       ])
     ]);
+  };
+
+  var monthMarkerViews = function(daysAgo, daysAfter, pixelsPerDay, height) {
+    var views = [];
+    var today = startOfDay();
+    for (var di = -daysAgo, dlen = daysAfter + 1; di < dlen; di++) {
+      var date = addDays(today, di);
+      if (date.getDate() !== 1)
+        continue;
+      var month = date.getMonth() + 1;
+      var showYear = (month === 1 || month === 4 || month === 7 || month === 10);
+      var x = (di + daysAgo) * pixelsPerDay;
+      var width = showYear ? 60 : 20;
+      var text = showYear ? date.getFullYear() + '/' + month : month;
+      views.push(m('g.month', [
+        m('line', {
+          x1: x,
+          y1: 0,
+          x2: x,
+          y2: height
+        }),
+        m('rect', {
+          x: x - (showYear ? 30 : 10),
+          y: 5,
+          width: width,
+          height: 20,
+          rx: 5,
+          ry: 5
+        }),
+        m('text', {
+          x: x,
+          y: 20
+        }, text)
+      ]));
+    }
+    return views;
   };
 
   app.timeAxisView = timeAxisView;
